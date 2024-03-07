0dc18f9aeb7f2fdc4e71a68d2697b7372170e0dc
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2024-03-07 16:36:52 -0500

    fix

--- app/api/test/route.tsx
+++ app/api/test/route.tsx
@@ -2,15 +2,19 @@ import { clerkClient } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 // testclerk123 userId: user_2cSSCzV7948rhPJMsY601tXsEU4
+// alexis@clerk.dev userId: user_2V7JJKmoA9HqzHhfMqK5cpgLl56
 
 export async function GET() {
-  const userId = "user_2cSSCzV7948rhPJMsY601tXsEU4";
+  const userId = "user_2V7JJKmoA9HqzHhfMqK5cpgLl56";
 
-  const params = { firstName: "John", lastName: "Wick" };
+  const provider = "oauth_google";
 
-  const response = await clerkClient.users.updateUser(userId, params);
+  const response = await clerkClient.users.getUserOauthAccessToken(
+    userId,
+    provider
+  );
 
   console.log(response);
 
-  return NextResponse.json({ message: "success" });
+  return NextResponse.json({ message: response });
 }
